6b788aa13ef2622f6fa48afb8ee7e139b9c844c7
gfhe <90042648+gfhe@users.noreply.github.com>
2022-11-23 23:19:37 +0800

    Update init.py

--- init.py
+++ init.py
@@ -4,6 +4,5 @@ import re
 import dl_translate as dlt
 
 ltp = LTP(pretrained_model_name_or_path='LTP/small')
-mt_m2m100 = dlt.TranslationModel(model_or_path='facebook/m2m100_418M',model_family='m2m100',device='cuda')
-"""2021/05/20新增mbart50模型"""
-mt_mbart50 = dlt.TranslationModel(model_or_path='facebook/mbart-large-50-many-to-many-mmt',model_family='mbart50',device='cuda')
+mt_m2m100 = dlt.TranslationModel(model_or_path='facebook/m2m100_418M',model_family='m2m100',device='cpu')
+mt_mbart50 = dlt.TranslationModel(model_or_path='facebook/mbart-large-50-many-to-many-mmt',model_family='mbart50',device='cpu')
